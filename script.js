635926276858a90e8b2d9d1a475072e1fe755ecd
Arif Hossan <srabon.eu.28@gmail.com>
2023-01-07 23:47:10 +0600

    using function handle increment and decrement value

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
 let itemOnePrice = 1219;
-let addItemOne = document.getElementById("addItemOne");
-addItemOne.addEventListener("click", function () {
-  let itemOneValue = increment("itemOne");
-  let itemOneTotalPrice = itemOnePrice * itemOneValue;
-  document.getElementById("itemOnePrice").innerText = itemOneTotalPrice;
-});
+let itemTwoPrice = 59;
 
-let removeItemOne = document.getElementById("removeItemOne");
-removeItemOne.addEventListener("click", function () {
-  let itemOneValue = getInputValue("itemOne");
-  if (itemOneValue > 1) {
-    itemOneValue--;
-    document.getElementById("itemOne").value = itemOneValue;
+/* item One plus button
+// let addItemOne = document.getElementById("addItemOne");
+// addItemOne.addEventListener("click", function () {
+//   let itemOneValue = increment("itemOne");
+//   updateSingleItemPrice("itemOnePrice", itemOnePrice, itemOneValue);
+// });*/
+/* item One minus button
+// let removeItemOne = document.getElementById("removeItemOne");
+// removeItemOne.addEventListener("click", function () {
+//   let itemOneValue = getInputValue("itemOne");
+//   if (itemOneValue > 1) {
+//     itemOneValue--;
+//     document.getElementById("itemOne").value = itemOneValue;
 
-    updateSingleItemPrice("itemOnePrice", itemOnePrice, itemOneValue);
-  } else {
-    alert("Item's quantity should be positive");
-  }
-});
+//     updateSingleItemPrice("itemOnePrice", itemOnePrice, itemOneValue);
+//   } else {
+//     alert("Item's quantity should be positive");
+//   }
+// });*/
+incrementItem("addItemOne", "itemOne", "itemOnePrice", itemOnePrice);
+decrementItem("removeItemOne", "itemOne", "itemOnePrice", itemOnePrice);
+
+incrementItem("addItemTwo", "itemTwo", "itemTwoPrice", itemTwoPrice);
+decrementItem("removeItemTwo", "itemTwo", "itemTwoPrice", itemTwoPrice);
+
+function incrementItem(btnId, itemQty, itemValueId, itemPrice) {
+  let addItem = document.getElementById(btnId);
+  addItem.addEventListener("click", function () {
+    let itemQuantity = increment(itemQty);
+    updateSingleItemPrice(itemValueId, itemPrice, itemQuantity);
+  });
+}
+
+function decrementItem(btnId, itemQty, itemValueId, itemPrice) {
+  let removeItem = document.getElementById(btnId);
+  removeItem.addEventListener("click", function () {
+    let itemValue = getInputValue(itemQty);
+    if (itemValue > 1) {
+      itemValue--;
+      document.getElementById(itemQty).value = itemValue;
+
+      updateSingleItemPrice(itemValueId, itemPrice, itemValue);
+    } else {
+      alert("Item's quantity should be positive");
+    }
+  });
+}
 
 function getInputValue(id) {
   let item = document.getElementById(id).value;
